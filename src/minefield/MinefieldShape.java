1a4347046c7a0754f28c8a4f047bdbcb43de717e
eyeOosh <aayushkumar1824@gmail.com>
2023-03-14 15:37:01 -0700

    minesquare class added + minefieldshape changed

--- src/minefield/MinefieldShape.java
+++ src/minefield/MinefieldShape.java
@@ -1,4 +1,13 @@
 package minefield;
 
+import java.awt.geom.Rectangle2D;
+
 public class MinefieldShape {
+
+    private Minefield minefield;
+
+    private int height, width, x, y;
+
+    private Rectangle2D.Double
+
 }
