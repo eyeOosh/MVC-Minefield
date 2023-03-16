00ce73766c5c00cc10a82543fa755a7733e4b0e6
eyeOosh <aayushkumar1824@gmail.com>
2023-03-16 12:01:04 -0700

    Updated minesquare

--- src/minefield/MineSquare.java
+++ src/minefield/MineSquare.java
@@ -1,5 +1,6 @@
 package minefield;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
@@ -15,6 +16,11 @@ public class MineSquare {
 
     private Rectangle2D.Double mine;
 
+    // number of adjacent mines
+    private char numMines;
+
+    private JTextArea text;
+
     public MineSquare(int x, int y, int w, int h, boolean isMined, boolean hasMine) {
         this.x = x;
         this.y = y;
@@ -23,6 +29,16 @@ public class MineSquare {
         this.isMined = isMined;
         this.hasMine = hasMine;
         mine = new Rectangle2D.Double(x, y, w, h);
+        numMines = '?';
+        text = new JTextArea(String.valueOf(numMines));
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     public double getW() {
@@ -45,10 +61,15 @@ public class MineSquare {
         return hasMine;
     }
 
+    public char getNumMines() {
+        return numMines;
+    }
+
     public void draw(Graphics2D gc) {
         Color oldColor = gc.getColor();
         gc.setColor(Color.GRAY);
         gc.draw(mine);
+        gc.draw();
     }
 
 }
