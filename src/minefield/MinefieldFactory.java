9407d6b8a7ca1777db3c1665bbc6e532c676ac5d
Hasnain Mucklai <mucklaihasnain@gmail.com>
2023-03-20 10:46:41 -0700

    completed MineFieldFactory

--- src/minefield/MinefieldFactory.java
+++ src/minefield/MinefieldFactory.java
@@ -1,32 +1,41 @@
 package minefield;
 //
 
-public class MinefieldFactory {
+import mvc.AppFactory;
 
+public class MinefieldFactory implements AppFactory {
+
+    @Override
     public Model makeModel() {
         return new Minefield();
     }
 
+    @Override
     public View makeView(Model m) {
         return new MinefieldView((Minefield) m);
     }
 
+    @Override
     public String getTitle() {
         return "Minefield Project Team 4";
     }
 
+    @Override
     public String[] getHelp() {
         return new String[]{"Click on a button to move in that direction."};
     }
 
+    @Override
     public String about() {
         return "CS151 Minefield Project Team 4";
     }
 
+    @Override
     public String[] getEditCommands() {
         return new String[]{"North", "South", "East", "West", "NorthEast", "NorthWest", "SouthEast", "SouthWest"};
     }
 
+    @Override
     public Command makeEditCommand(Model model, String name, Object source) {
         if (name == "North")
             return new MoveCommand(model);
